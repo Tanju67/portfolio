967892b5ecc411d2b7d2f07ed91bfa06b4fecfe7
Tanju Özer <tanjuozer64@gmail.com>
2024-07-19 20:42:58 +0200

    handle route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./page/RootLayout";
+import ErrorPage from "./page/ErrorPage";
+import HomePage from "./page/HomePage";
+import AboutPage from "./page/AboutPage";
+import ProjectsPage from "./page/ProjectsPage";
+import ContactPage from "./page/ContactPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/projects", element: <ProjectsPage /> },
+      { path: "/contact", element: <ContactPage /> },
+    ],
+  },
+]);
 
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/shared/navigation/Navigation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Navigation.module.css";
+
+function Navigation() {
+  return <div>Navigation</div>;
+}
+
+export default Navigation;
